20d25f7a335cc9ded956eebb32923145ceda30cb
amanda-j-brown <145283004+amanda-j-brown@users.noreply.github.com>
2023-12-06 12:48:40 -0500

    Create Day2partone.py

---
+++ Day2partone.py
@@ -0,0 +1,42 @@
+def matching_games(game, red, green, blue):
+  string_to_list = game.split(" ")
+  blue_list = []
+  red_list = []
+  green_list = []
+  for i in range(len(string_to_list)):
+    if string_to_list[i].startswith('blue'):
+      blue_list.append(int(string_to_list[i-1]))
+    elif string_to_list[i].startswith('green'):
+      green_list.append(int(string_to_list[i-1]))
+    elif string_to_list[i].startswith('red'):
+      red_list.append(int(string_to_list[i-1]))
+
+  for num in blue_list:
+    if num > blue:
+      return False
+
+  for num in red_list:
+    if num > red:
+      return False
+
+  for num in green_list:
+    if num > green:
+      return False
+
+  if len(string_to_list[1]) == 2:
+    return int(string_to_list[1][0])
+  else:
+    return int(string_to_list[1][:2])
+
+
+possible_games = []
+with open('games.txt') as lines_doc:
+  for single_game in lines_doc.readlines():
+    result = matching_games(single_game, 12, 13, 14)
+    if result == False:
+      continue
+    else:
+      possible_games.append(result)
+
+print(possible_games)
+print(sum(possible_games))
